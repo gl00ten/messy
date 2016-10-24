9b09c3c41c94aa017c76f1522fd2731eee98a71e
Miguel <fullmooninu@users.noreply.github.com>
2016-10-24 18:24:44 +0100

    Update uniformizingTest.pp

--- uniformizingTest.pp
+++ uniformizingTest.pp
@@ -1,7 +1,7 @@
 
 class puppetEnvironmentTest {
   # stuff missing from virtual machines
-  $wantedPackages = ['ntp','curl','htop','ssh', 'sshfs', 'git','nmap','hexchat','libreoffice','pulseaudio','workrave','virtualbox']
+  $wantedPackages = ['ntp','curl','htop','ssh', 'sshfs', 'git','nmap','hexchat','libreoffice','pulseaudio','workrave','virtualbox','keepassx']
   package { $wantedPackages: ensure => installed }
  
  
